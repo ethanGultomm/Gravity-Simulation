c5d36bf1a30188d579a00eced16d4ff873761877
Ethan Gultom <e.gultom@student.fontys.nl>
2026-02-19 03:29:19 +0100

    i made it draw a circle

--- src/GravitySim.cpp
+++ src/GravitySim.cpp
@@ -4,6 +4,7 @@
 #include <glm/glm.hpp>
 
 #include <iostream>
+#include <vector>
 #include <stdlib.h>
 #include <cmath>
 using namespace std;;
@@ -31,8 +32,6 @@ void processInput(GLFWwindow* window){
     }
 }
 
-GLfloat circleVertices;
-
 int main(){
     glfwSetErrorCallback(error_callback);
 
@@ -43,7 +42,7 @@ int main(){
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    GLFWwindow* window = glfwCreateWindow(800, 600, "cock-and-ball-inator 3000", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(800, 800, "cock-and-ball-inator 3000", NULL, NULL);
     if (!window){
         glfwTerminate();
         cerr << "creating window failed" << endl;
@@ -66,6 +65,35 @@ int main(){
         0.0f, 0.5f, 0.0f
     };
 
+    // draw a circle with a given resolution
+    int const res = 50;
+    std::vector<GLfloat> center = {0.0f, 0.0f, 0.0f};
+    GLfloat radius = 0.3f;
+
+    GLfloat circleVertices[(res+1)*3];
+    circleVertices[0] = center[0];
+    circleVertices[1] = center[1];
+    circleVertices[2] = center[2];
+    GLuint indices[res*3];
+
+    for (size_t i = 0; i < res; i++)
+    {
+        // calculate the next vertex with trigonometry
+        GLfloat angle = 2.0f * 3.14159265359 * (static_cast<float>(i) / res);
+        GLfloat x = center[0] + cos(angle) * radius;
+        GLfloat y = center[1] + sin(angle) * radius;
+        // save the vertex, its i+1 because index 0 is already used by the center point
+        circleVertices[(i+1)*3] = x;
+        circleVertices[((i+1)*3)+1] = y;
+        circleVertices[((i+1)*3)+2] = 0.0f; // since we're working with 2d, Z axis stays at 0
+
+        // make the indices, look at sketch.jpg for uhh some kinda explanation lol
+        indices[i*3]     = 0;   // indice a
+        indices[(i*3)+1] = i+1; // indice b
+        indices[(i*3)+2] = i+2; // indice c
+    }
+    indices[(res*3)-1] = 1; // connects the last triangle to the first vertex
+
     GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
     glCompileShader(vertexShader);
@@ -83,21 +111,26 @@ int main(){
     glDeleteShader(fragmentShader);
 
 
-    GLuint VAO, VBO;
+    GLuint VAO, VBO, EBO;
 
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
 
     glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(circleVertices), circleVertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
     int success;
     char infolog[512];
@@ -117,7 +150,7 @@ int main(){
 
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, res*3, GL_UNSIGNED_INT, 0);
 
         // check and call events and swap the buffers
         glfwSwapBuffers(window);
@@ -126,6 +159,7 @@ int main(){
 
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
     glDeleteProgram(shaderProgram);
 
     glfwDestroyWindow(window);
